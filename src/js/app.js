6a49f2d8cd1426bde813177902e6f848dbc9a9b2
vertalet76crew <vertalet76crew@gmail.com>
2020-12-28 09:54:35 +0400

    INIT

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+import 'polyfill-array-includes';
+import 'element-closest-polyfill';
+import $ from 'jquery';
+
+import menu from './blocks/menu';
+import formRequest from './blocks/form-request';
+import newsCarousel from './blocks/news';
+import faq from './blocks/faq';
+
+$(function() {
+    const patternUserAgent = /iPhone|iPod|iPad|iOS|android/i;
+    const isMobile = !!navigator.userAgent.match(patternUserAgent);
+    menu(isMobile);
+    formRequest(isMobile);
+    newsCarousel();
+    faq();
+});
